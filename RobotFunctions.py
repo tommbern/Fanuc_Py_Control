348bf55811a74b995f6e9fd12e7355b98c60209b
tommbern <tommbern@gmail.com>
2023-11-20 17:18:42 +0100

    20.11.2023

---
+++ RobotFunctions.py
@@ -0,0 +1,177 @@
+from fanucpy import Robot
+import numpy as np
+import requests
+import roboticstoolbox as rtb
+import spatialmath as sm
+
+
+esp32_ip = "192.168.4.1"                    # ESP32 IP address
+relay_on_url = f"http://{esp32_ip}/L"
+relay_off_url = f"http://{esp32_ip}/H"
+
+
+def control_relay(action):
+    response = requests.get(relay_on_url if action == "on" else relay_off_url)
+    if response.status_code == 200:
+        print(f"Relay turned {action.upper()}")
+    else:
+        print(f"Failed to turn {action.upper()} relay")
+
+
+def LRMate200iD4S_gen():  # Define the LRMate200iD4S robot using roboticstoolbox
+    # Define DH parameters for your specific robot model
+    # Replace these with the correct values for your robot
+    dh_params = [
+        # rtb.RevoluteDH(d=0.330, alpha=np.pi / 2),
+        rtb.RevoluteDH(alpha=-np.pi / 2),
+        rtb.RevoluteDH(a=0.260, alpha=np.pi, offset=-np.pi / 2),
+        rtb.RevoluteDH(a=-0.02, alpha=np.pi / 2, offset=-np.pi),
+        rtb.RevoluteDH(d=-0.290, alpha=-np.pi / 2),
+        rtb.RevoluteDH(alpha=-np.pi / 2, offset=-np.pi),
+        rtb.RevoluteDH(d=-0.07, alpha=-np.pi)
+    ]
+
+    # Create the robot model
+    robot = rtb.DHRobot(dh_params, name="LRMate200iD4s")
+    # robot.tool = sm.SE3(0.25, 0, 0.2) * sm.SE3.Ry(np.pi)
+    # robot.base = sm.SE3(0, 0, -0.3)
+    # print(robot)
+    return robot
+
+
+lrmate = LRMate200iD4S_gen()
+
+
+def joints_fanuc2corke(q):
+    if q.ndim == 1:
+        q = [q]
+    q_adj = q
+    q_adj[:, 2] = q[:, 2] + q[:, 1]
+    q_adj = q_adj / 180 * np.pi
+    return q_adj
+
+
+def corke2fanuc(q):
+    q_adj = np.array(q)
+    if q_adj.ndim == 1:
+        q_adj = q_adj[None, :]              # Convert 1D array to 2D
+    q_adj[:, 2] = q_adj[:, 2] - q_adj[:, 1]
+    q_adj = q_adj / np.pi * 180
+    return q_adj
+
+
+def move_robot_cartesian(lrmate, fanuc_robot):
+    try_pose = []
+    try:
+        current_joint_positions = fanuc_robot.get_curjpos()                                                             # si legge la posizione dei joint
+        print("Current Joint Positions:", current_joint_positions)
+        current_cartesian_positions = fanuc_robot.get_curpos()                                                          # si legge la posizione cartesiana (non conosciamo la base)
+        print("Current Cartesian Positions:", current_cartesian_positions)
+
+        x_translation_input = input("Enter X translation value in millimeters (press Enter for 0): ")
+        x_translation = float(x_translation_input) / 1000 if x_translation_input else 0
+        y_translation_input = input("Enter Y translation value in millimeters (press Enter for 0): ")
+        y_translation = float(y_translation_input) / 1000 if y_translation_input else 0
+        z_translation_input = input("Enter Z translation value in millimeters (press Enter for 0): ")
+        z_translation = float(z_translation_input) / 1000 if z_translation_input else 0
+
+        # Cinematica Diretta
+        joint_angles = np.zeros([1, 6])
+        joint_angles[0, :] = current_joint_positions
+        current_joint_positions_1 = joints_fanuc2corke(joint_angles)                                                    # trasformazione angoli joint per sfruttare il toolbox di Corke
+        current_fkine_position = lrmate.fkine(current_joint_positions_1)
+        print("Current fkine Positions:", current_fkine_position)                                                       # con la cinematica diretta si trova la posizione cartesiana secondo la base definita qui
+
+        # Cinematica Inversa
+        euler_angles = sm.SE3.rpy(current_fkine_position, 'deg', 'zyx')                                     # si ottengono gli angoli di eulero per yaw-pitch-roll partendo dalla cinematica diretta
+        rotation_matrix = sm.SO3.RPY([euler_angles[0], euler_angles[1], euler_angles[2]], unit="deg")           # viene definita la matrice di rotazione con gli angoli di eulero
+        try_pose = sm.SE3(rotation_matrix)                                                                              # si crea la matrice di trasformazione omogenea, vengono inseriti i valore di orientamento
+        try_pose.A[0, 3] = current_fkine_position.A[0, 3] + x_translation  # Add user input to X position
+        try_pose.A[1, 3] = current_fkine_position.A[1, 3] + y_translation  # Add user input to Y position
+        try_pose.A[2, 3] = current_fkine_position.A[2, 3] + z_translation  # Add user input to Z position                                               # si inserisce la posizione cartesiana Z
+        current_ikine_position = lrmate.ikine_LM(try_pose, q0=current_joint_positions)                                  # si calcola la cinematica inversa
+
+        current_ikine_position_deg = corke2fanuc(current_ikine_position.q)                                              # trasformazione in base Fanuc (da Corke)
+        print("Current ikine values deg:", current_ikine_position_deg)
+
+        original_value = np.array(current_ikine_position_deg)
+        rounded_value = np.round(original_value, decimals=2)
+        print(rounded_value)
+        transformed_values = rounded_value.tolist()[0]
+        new_joint_positions = transformed_values                                                                        # si crea la nuova lista di Joint-position da fornire al robot.move()
+        print(new_joint_positions)
+
+        # control_relay("on")
+        fanuc_robot.move("joint", vals=new_joint_positions, velocity=5, acceleration=50, cnt_val=0, linear=False)
+        # control_relay("off")
+
+    except Exception as e:
+        print(f"Failed to move: {str(e)}")
+
+
+def move_robot_cartesian_large_displacement(lrmate, fanuc_robot, x_translation, y_translation, z_translation):
+    try:
+        max_translation = max(abs(x_translation), abs(y_translation), abs(z_translation)) * 1000
+        joint_positions_list = []
+
+        for i in range(int(max_translation)):
+            if i == 0:
+                current_joint_positions = fanuc_robot.get_curjpos()
+            else:
+                current_joint_positions = joint_positions_list[-1]
+
+            joint_angles = np.zeros([1, 6])
+            joint_angles[0, :] = current_joint_positions
+            current_joint_positions_1 = joints_fanuc2corke(joint_angles)
+            current_fkine_position = lrmate.fkine(current_joint_positions_1)
+
+            try_pose = sm.SE3(current_fkine_position.A)
+            increment_x = x_translation / max_translation
+            increment_y = y_translation / max_translation
+            increment_z = z_translation / max_translation
+            try_pose.A[0, 3] += increment_x
+            try_pose.A[1, 3] += increment_y
+            try_pose.A[2, 3] += increment_z
+
+            current_ikine_position = lrmate.ikine_LM(try_pose, q0=current_joint_positions)
+            current_ikine_position_deg = np.round(corke2fanuc(current_ikine_position.q), decimals=5)
+
+            joint_positions_list.append(current_ikine_position_deg[0].tolist())
+            print(f"Iteration {i + 1}: Joint positions - {current_ikine_position_deg}")
+
+        return joint_positions_list
+
+    except Exception as e:
+        print(f"Failed to move: {str(e)}")
+        return []
+
+
+def move_robot_with_joint_positions(robot, joint_positions_list):
+    try:
+        # control_relay("on")
+
+        for joint_positions in joint_positions_list:
+            robot.move("joint", vals=joint_positions, velocity=20, acceleration=100, cnt_val=50, linear=False)
+
+        # control_relay("off")
+
+    except Exception as e:
+        print(f"Failed to move: {str(e)}")
+
+
+def move_robot_joint(robot, joint_index, degrees, velocity=5):
+    try:
+        current_joint_positions = robot.get_curjpos()
+        joint_positions = current_joint_positions.copy()
+        joint_positions[joint_index] += degrees
+        robot.move(
+            "joint",
+            vals=joint_positions,
+            velocity=velocity,  # Reduced velocity
+            acceleration=50,
+            cnt_val=0,
+            linear=True
+        )
+        print(f"Moved joint {joint_index + 1} by {degrees} degrees.")
+    except Exception as e:
+        print(f"Failed to move joint {joint_index + 1}: {str(e)}")
